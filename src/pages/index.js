aab8f806a520643050362d48e0102f9ac828d609
claudina sarahe <cs@misscs.me>
2018-01-19 21:52:21 -0400

    Add basic Trust module

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
+import Trust from "../components/Trust/Trust";
 
 
 const IndexPage = () => (
@@ -8,6 +9,10 @@ const IndexPage = () => (
       headline="Know what your society is thinking"
       subheadline="Polis is a social research platform that helps you gather insights."
       className="page-header"
+    />
+    <Trust
+      headline="You’re in good company"
+      subheadline="Polis is trusted by governments, universities, non-profits, movements, and large organizations."
     />
   </div>
 );

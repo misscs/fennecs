2f3c43ee3a0c0bfb7729fd41ff411dd5d9de0be4
Claudina Sarahe <cs@compositescollective.com>
2018-05-23 06:14:12 -0400

    Remove link to request demo

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,23 +3,11 @@ import Logo from "../Core/Logo/Logo";
 import { throttle } from "lodash";
 
 const t = {
-  cta: "Requst a Demo",
+  create: "Create an Account",
   signin: "Sign In"
 };
 
 class Nav extends React.Component {
-  // componentDidMount() {
-  //   window.addEventListener("scroll", throttle(this.onScroll.bind(this), 200));
-  // }
-
-  // onScroll(event) {
-  //   const elem = document.getElementById("cta-button");
-  //   if (elem && window.scrollY > elem.offsetTop + elem.offsetHeight) {
-  //     this.button.classList.remove("btn-ghost");
-  //   } else {
-  //     this.button.classList.add("btn-ghost");
-  //   }
-  // }
 
   render() {
     return <nav role="banner" className="fixed bg-blue top-0 left-0 right-0 bg-white z-1 w-100 ph3 ph4-ns pv3">
@@ -28,8 +16,8 @@ class Nav extends React.Component {
           <a href="https://pol.is/home/signin" className="link mr4 white">
             {t.signin}
           </a>
-          <a href="/demo/" ref={button => (this.button = button)} className="br2 ba ph2 pv2  tc white   w-100 w-auto-ns   mt3 mt0-ns link">
-            {t.cta}
+          <a href="https://pol.is/home/signin" className="link mr4 white">
+            {t.create}
           </a>
         </div>
       </nav>;
